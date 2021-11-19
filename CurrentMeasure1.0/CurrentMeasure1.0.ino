8cd6d50e3f0da795fa786106e22ba707fcb03716
dhikihandika <dhikihandika36@gmail.com>
2021-11-19 16:10:57 +0700

    "current and environtment measure"

---
+++ CurrentMeasure1.0/CurrentMeasure1.0.ino
@@ -0,0 +1,51 @@
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//-------------------------------                                     Current Sensor Measure Module                                      ----------------------------//
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+#include "EmonLib.h"                            //Include Emon Library 
+EnergyMonitor emon0;                            // Create an instance emon0
+EnergyMonitor emon1;                            // Create an instance emon1
+EnergyMonitor emon2;                            // Create an instance emon2
+EnergyMonitor emon3;                            // Create an instance emon3
+EnergyMonitor emon4;                            // Create an instance emon4
+EnergyMonitor emon5;                            // Create an instance emon5
+EnergyMonitor emon6;                            // Create an instance emon6
+EnergyMonitor emon7;                            // Create an instance emon7
+EnergyMonitor emon8;                            // Create an instance emon8
+
+void setup() {
+    Serial.begin(9600);                         // Serial begin initialization
+    emon0.current(0, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon1.current(1, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon2.current(2, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon3.current(3, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon4.current(4, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon5.current(5, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon6.current(6, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon7.current(7, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+    emon8.current(8, 47);                       // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62(in resistor) + 100(ex resistor) = 47.
+ } 
+
+void loop() {
+    uint8_t Irms0 = emon0.calcIrms(1800);      // Calculate Irms0 only 
+    uint8_t Irms1 = emon1.calcIrms(1800);      // Calculate Irms1 only   
+    uint8_t Irms2 = emon2.calcIrms(1800);      // Calculate Irms2 only   
+    uint8_t Irms3 = emon3.calcIrms(1800);      // Calculate Irms3 only   
+    uint8_t Irms4 = emon4.calcIrms(1800);      // Calculate Irms4 only   
+    uint8_t Irms5 = emon5.calcIrms(1800);      // Calculate Irms5 only   
+    uint8_t Irms6 = emon6.calcIrms(1800);      // Calculate Irms6 only   
+    uint8_t Irms7 = emon7.calcIrms(1800);      // Calculate Irms7 only   
+    uint8_t Irms8 = emon8.calcIrms(1800);      // Calculate Irms8 only      
+
+    Serial.print("|Irms0=");Serial.print(Irms0);// Irms0  
+    Serial.print("|Irms1=");Serial.print(Irms1);// Irms1 
+    Serial.print("|Irms2=");Serial.print(Irms2);// Irms2 
+    Serial.print("|Irms3=");Serial.print(Irms3);// Irms3 
+    Serial.print("|Irms4=");Serial.print(Irms4);// Irms4 
+    Serial.print("|Irms5=");Serial.print(Irms5);// Irms5 
+    Serial.print("|Irms6=");Serial.print(Irms6);// Irms6 
+    Serial.print("|Irms7=");Serial.print(Irms7);// Irms7 
+    Serial.print("|Irms8=");Serial.println(Irms8);// Irms8  
+    Serial.println("----------------------------------------------------------------------------------------");
+    delay(100);
+}
---
+++ EnvirontmentSensor1.0/EnvirontmentSensor1.0.ino
@@ -0,0 +1,50 @@
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//-------------------------------                                     Voltage Sensor Measure Module                                      ----------------------------//
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+#include <Wire.h>                                                         // Libraries communication protocol I2C
+#include <Adafruit_BME280.h>                                              // libraries BME280 sensor
+#include <Adafruit_Sensor.h>                                              // libraries BME280 sensor
+
+#define SEALEVELPRESSURE_HPA (1013.25)                                    // First value for pressure
+
+uint64_t oldtime = 0;
+Adafruit_BME280 bme;                                                      // Access I2C
+
+void setup() {
+  Serial.begin(9600);                                                     // Serial initialization
+ 
+  if (!bme.begin(0x77)) {
+    Serial.println("BME280 sensor can't detceted, check your address or circuit!");
+    while (1);
+  } else {
+    Serial.println("BME280 sensor detected!");
+  }
+}
+ 
+void loop() { 
+  if((millis() - oldtime)>=1000){
+      //Temperature read
+      Serial.print("Temperature = "); 
+      Serial.print(bme.readTemperature());
+      Serial.println(" *C"); 
+     
+      //Humidity read
+      Serial.print("Humidity = "); 
+      Serial.print(bme.readHumidity()); 
+      Serial.println(" %"); 
+       
+      //Pressure read
+      Serial.print("Pressure = ");
+      Serial.print(bme.readPressure() / 100.0F);
+      Serial.println(" hPa");
+       
+      //Altitude read based on surface or sea
+      Serial.print("Approx. Altitude = ");
+      Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+      Serial.println(" m");
+       
+      Serial.println("-----------------------------");
+      oldtime = millis();
+  }
+}
